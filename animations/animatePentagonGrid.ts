21bd307647fb82e6afdd7e8c447e9c0669e50069
Max Hernandez <maxhernandezcastillo@gmail.com>
2023-02-08 02:37:16 -0600

    Calculated vertexes of hexagons in grid

--- animations/animatePentagonGrid.ts
+++ animations/animatePentagonGrid.ts
@@ -57,80 +57,39 @@ class PentagonGrid {
           + (i * 3 * this.grid.cell.size.radius)
           + this.grid.cell.size.diameter
         )
-        if (!hexagons.get(y)?.get(x)) {
-          hexagons.set(x, new Map())
-        }
-
         hexagons.get(y)?.set(x, {
           center: {x, y},
-          vertices: new Array()
-        })
-        /*
-        centers.push({
-          x: Math.floor(
-            (j % 2 === 0 ? 1.5 * this.grid.cell.size.radius : 0)
-            + (i * 3 * this.grid.cell.size.radius)
-            + this.grid.cell.size.diameter
-          ),
-          y
-        })
-        */
-      }
-    }
-
-    /*
-    const centers: {x: number, y: number}[] = Array()
-    for (let j = 0; j < 50; j++) {
-      for (let i = 0; i < 50; i++) {        
-        centers.push({
-          x: Math.floor(
-            (j % 2 === 0 ? 1.5 * this.grid.cell.size.radius : 0)
-            + (i * 3 * this.grid.cell.size.radius)
-            + this.grid.cell.size.diameter
-          ),
-          y: Math.floor(j * this.grid.cell.size.radius)
-
+          vertices: [
+            {x: x + this.grid.cell.size.radius, y: y},
+            {x: x - this.grid.cell.size.radius, y: y},
+            {x: x - this.grid.cell.size.radius * 0.5, y: y - this.grid.cell.size.radius},
+            {x: x - this.grid.cell.size.radius * 0.5, y: y + this.grid.cell.size.radius},
+            {x: x + this.grid.cell.size.radius * 0.5, y: y - this.grid.cell.size.radius},
+            {x: x + this.grid.cell.size.radius * 0.5, y: y + this.grid.cell.size.radius},
+          ]
         })
       }
     }
-    */
 
-    /*
-    const vertices: {x: number, y: number}[] = Array()
-    for (let j = 0; j < 50; j++) {
-      for (let i = 0; i < 50; i++) {
-        vertices.push({
-          x: Math.floor(
-            (Math.floor(i / 2) * this.grid.cell.size.radius) 
-            + (Math.floor(i / 2) * this.grid.cell.size.diameter) 
-            + ((i % 2 === 0 ? 1 : 0) * this.grid.cell.size.radius)
-            + ((j % 2 === 0 ? 1 : 0) * this.grid.cell.size.radius * 1.5)
-          ),
-          y: Math.floor(j * this.grid.cell.size.radius)
-        })
+    for (const y of hexagons.keys()) {
+      // @ts-ignore
+      for (const x of hexagons.get(y).keys()) {
+        context.beginPath()
+        context.arc(x, y, 2, 0, 2 * Math.PI, false)
+        context.fillStyle = '#FFFFFFFF'
+        context.fill()
+        context.closePath()
+
+        // @ts-ignore
+        for (const vertex of hexagons.get(y)?.get(x)?.vertices) {
+          context.beginPath()
+          context.arc(vertex.x, vertex.y, 1, 0, 2 * Math.PI, false)
+          context.fillStyle = '#FFFFFF30'
+          context.fill()
+          context.closePath()  
+        }
       }
     }
-    */
-
-    /*
-    console.log('CENTERS', centers.length)
-    console.log('VERTICES', vertices.length)
-
-    for (let i = 0; i < vertices.length; i++) {
-      context.beginPath()
-      context.fillStyle = '#FFFFFF60'  
-      context.arc(vertices[i].x, vertices[i].y, 1, 0, 2 * Math.PI)
-      context.fill()
-      context.closePath()
-    }
-    for (let i = 0; i < centers.length; i++) {
-      context.beginPath()
-      context.fillStyle = 'white'  
-      context.arc(centers[i].x, centers[i].y, 2, 0, 2 * Math.PI)
-      context.fill()
-      context.closePath()
-    }
-    */
   }
   drawHexagon(center: {x: number, y: number}) {
     const context = this.canvas.getContext("2d")
