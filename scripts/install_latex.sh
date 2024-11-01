18e5c66c8a1befaf685dadfd89f0fd7611a833f9
Max Hernandez <maxhernandezcastillo@gmail.com>
2024-11-01 16:32:43 -0600

    going back to custom installing script for tinytex

--- scripts/install_latex.sh
+++ scripts/install_latex.sh
@@ -10,15 +10,15 @@ sudo apt install -y \
   libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev build-essential chrpath libssl-dev libxft-dev
 # make sure perl is properly installed (e.g., apt install -y perl)
 perl -mFile::Find /dev/null
-# then install TinyTeX
 
+# then install TinyTeX
 TINYTEX_URL=https://github.com/rstudio/tinytex-releases/releases/download/daily/TinyTeX-1
 wget --retry-connrefused --progress=dot:giga -O TinyTeX.tar.gz ${TINYTEX_URL}.tar.gz
 
 tar xf TinyTeX.tar.gz -C "$HOME"
 rm TinyTeX.tar.gz
 
-ln -s $HOME/.TinyTeX/bin/*/* /usr/local/bin/ 
+sudo ln -s $HOME/.TinyTeX/bin/*/* /usr/local/bin/ 
 
 if [ -z "$GITHUB_WORKSPACE" ]; then
   xargs tlmgr install < "$GITHUB_WORKSPACE"/content/resume/latex/modules.txt
